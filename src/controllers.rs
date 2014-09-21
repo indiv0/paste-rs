0f67b37e7281849eba829fbca555f1fe6125652a
Nikita Pekin <contact@nikitapek.in>
2014-09-20 20:30:07 -0400

    Get unicode working

--- src/controllers.rs
+++ src/controllers.rs
@@ -1,3 +1,4 @@
+use http::headers::content_type::MediaType;
 use http::status::NotFound;
 use nickel::{
     Action,
@@ -23,13 +24,12 @@ pub fn post_note(request: &Request, response: &mut Response) {
     response.set_content_type("application/x-www-form-urlencoded");
 
     let body = request.origin.body.as_slice();
-    println!("{}", body);
     let data = match body.split('&')
                          .filter(|x| x.as_slice()
                                       .starts_with("data"))
                          .next() {
         Some(x) => match x.split('=').skip(1).next() {
-            Some(value) => percent_encoding::percent_decode(value.as_bytes()),
+            Some(value) => percent_encoding::lossy_utf8_percent_decode(value.as_bytes()),
             None        => {
                 response.send(r#"{ "error": "failed to process form" }"#);
                 return
@@ -41,8 +41,6 @@ pub fn post_note(request: &Request, response: &mut Response) {
         }
     };
 
-    println!("{}", data);
-
     let code = util::random_string(settings::RANDOM_CODE_LENGTH);
     let mut note = Note::new(0, code.clone(), time::now_utc().to_timespec(), data.to_string());
 
@@ -52,7 +50,12 @@ pub fn post_note(request: &Request, response: &mut Response) {
 }
 
 pub fn get_note(request: &Request, response: &mut Response) {
-    response.set_content_type("text/plain");
+    response.origin.headers.content_type = Some(MediaType {
+        type_: "text".to_string(),
+        subtype: "plain".to_string(),
+        parameters: vec!((String::from_str("charset"), String::from_str("UTF-8")))
+    });
+    //response.set_content_type("html;charset=UTF-8");
 
     let code = request.params.index(&"code".to_string());
 
